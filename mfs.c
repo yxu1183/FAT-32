033825d157ade6b98c9f5637d14e3ded58bfe3ee
iamyunika <yupadhayaya08@gmail.com>
2020-05-01 17:53:45 -0500

    opens the image file

--- mfs.c
+++ mfs.c
@@ -1,25 +1,3 @@
-// The MIT License (MIT)
-// 
-// Copyright (c) 2016, 2017 Trevor Bakker 
-// 
-// Permission is hereby granted, free of charge, to any person obtaining a copy
-// of this software and associated documentation files (the "Software"), to deal
-// in the Software without restriction, including without limitation the rights
-// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
-// copies of the Software, and to permit persons to whom the Software is
-// furnished to do so, subject to the following conditions:
-// 
-// The above copyright notice and this permission notice shall be included in
-// all copies or substantial portions of the Software.
-// 
-// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
-// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
-// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
-// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
-// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
-// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
-// THE SOFTWARE.
-
 #define _GNU_SOURCE
 
 #include <stdio.h>
@@ -29,6 +7,8 @@
 #include <errno.h>
 #include <string.h>
 #include <signal.h>
+#include <ctype.h>
+#include <stdint.h>
 
 #define WHITESPACE " \t\n"      // We want to split our command line up into tokens
                                 // so we need to define what delimits our tokens.
@@ -39,8 +19,78 @@
 
 #define MAX_NUM_ARGUMENTS 5     // Mav shell only supports five arguments
 
+#define MAX_IMAGE_FILE_NAME 50
+
+/* struct declaration for Directory*/
+struct __attribute__((__packed__)) DirectoryEntry
+{
+  char DIR_Name[11];
+  uint8_t DIR_Attr;
+  uint8_t Unused1[8];
+  uint16_t DIR_FirstClusterHigh;
+  uint8_t Unused2[4];
+  uint16_t DIR_FirstClusterLow;
+  uint32_t DIR_FileSize;
+};
+
+FILE *ptr_file;
+
+char BS_OEMName[8];
+int16_t BPB_BytesPerSec;
+int8_t BPB_SecPerClus;
+int16_t BPB_RsvdSecCnt;
+int8_t BPB_NumFATs;
+int16_t BPB_RootEntCnt;
+char BS_VolLab[11];
+int32_t BPB_FATSz32;
+int32_t BPB_RootClus;
+
+int32_t RootDirSectors = 0; 
+int32_t FirstDataSector = 0;
+int32_t FirstSectorofCluster = 0;
+int32_t root_address;
+int file_open = 0;
+
+int LBAToOffset(int sector)
+{
+  return ((sector -2) * BPB_BytesPerSec)+(BPB_BytesPerSec* BPB_RsvdSecCnt)+(BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
+}
+
+int compare(char IMG_Name[MAX_IMAGE_FILE_NAME],char input[MAX_IMAGE_FILE_NAME] )
+{
+  char expanded_name[12];
+  memset( expanded_name, ' ', 12 );
+
+  char *token = strtok( input, "." );
+
+  strncpy( expanded_name, token, strlen( token ) );
+
+  token = strtok( NULL, "." );
+
+  if( token )
+  {
+    strncpy( (char*)(expanded_name+8), token, strlen(token ) );
+  }
+
+  expanded_name[11] = '\0';
+
+  int i;
+  for( i = 0; i < 11; i++ )
+  {
+    expanded_name[i] = toupper( expanded_name[i] );
+  }
+
+  if( strncmp( expanded_name, IMG_Name, 11 ) == 0 )
+  {
+    printf("They matched\n");
+  }
+
+  return 0;
+}
+
 int main()
 {
+  struct DirectoryEntry dir[16];
 
   char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
 
@@ -87,14 +137,66 @@ int main()
     // Now print the tokenized input as a debug check
     // \TODO Remove this code and replace with your shell functionality
 
-    int token_index  = 0;
-    for( token_index = 0; token_index < token_count; token_index ++ ) 
+    // int token_index  = 0;
+    // for( token_index = 0; token_index < token_count; token_index ++ ) 
+    // {
+    //   printf("token[%d] = %s\n", token_index, token[token_index] );  
+    // }
+    if(token[0] == NULL)
     {
-      printf("token[%d] = %s\n", token_index, token[token_index] );  
+      continue;
     }
 
-    free( working_root );
+    else if(strcmp("open",token[0])== 0)
+    {
+      if(token[1]==NULL)
+      {
+        printf("Error: Please enter the name of file to open.\n");
+        continue;
+      }
+      else
+      {
+        ptr_file = fopen(token[1],"r");
+        if(ptr_file == NULL)
+        {
+          printf("Error: No file system with the given name is found.\n");
+          continue;
+        }
+        if(file_open == 1)
+        {
+          printf("Error: File system with the given names is alread open.\n");
+          continue;
+        }
+        
+        fseek(ptr_file,11,SEEK_SET);
+        fread(&BPB_BytesPerSec, 2, 1, ptr_file);
 
+        fseek(ptr_file, 13, SEEK_SET);
+        fread(&BPB_SecPerClus, 1, 1, ptr_file);
+
+        fseek(ptr_file, 14, SEEK_SET);
+        fread(&BPB_RsvdSecCnt, 1, 2, ptr_file); 
+
+        fseek(ptr_file, 16, SEEK_SET);
+        fread(&BPB_NumFATs, 1, 1, ptr_file);
+
+        fseek(ptr_file, 36, SEEK_SET);
+        fread(&BPB_FATSz32, 1, 4, ptr_file);
+
+        root_address = (BPB_BytesPerSec* BPB_RsvdSecCnt)+(BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
+
+        fseek(ptr_file,root_address,SEEK_SET);
+        int i = 0;
+        for(i = 0; i < 16; i++)
+        {
+          fread(&dir[i],sizeof(dir[i]),1,ptr_file);  
+        }
+
+        printf("File successfully opened!!\n");
+        file_open = 1;   
+      }
+    }
+    free( working_root );
   }
   return 0;
 }
