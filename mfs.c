93a9777dded6e73ef0a2429cbf62822a8639c532
iamyunika <yupadhayaya08@gmail.com>
2020-05-02 21:39:26 -0500

    close function finally works

--- mfs.c
+++ mfs.c
@@ -10,16 +10,16 @@
 #include <ctype.h>
 #include <stdint.h>
 
-#define WHITESPACE " \t\n"      // We want to split our command line up into tokens
-                                // so we need to define what delimits our tokens.
-                                // In this case  white space
-                                // will separate the tokens on our command line
+#define WHITESPACE " \t\n" // We want to split our command line up into tokens \
+                           // so we need to define what delimits our tokens.   \
+                           // In this case  white space                        \
+                           // will separate the tokens on our command line
 
-#define MAX_COMMAND_SIZE 255    // The maximum command-line size
+#define MAX_COMMAND_SIZE 255 // The maximum command-line size
 
-#define MAX_NUM_ARGUMENTS 5     // Mav shell only supports five arguments
+#define MAX_NUM_ARGUMENTS 5 // Mav shell only supports five arguments
 
-#define MAX_IMAGE_FILE_NAME 50
+#define MAX_SIZE_FILE 50
 
 /* struct declaration for Directory*/
 struct __attribute__((__packed__)) DirectoryEntry
@@ -45,75 +45,77 @@ char BS_VolLab[11];
 int32_t BPB_FATSz32;
 int32_t BPB_RootClus;
 
-int32_t RootDirSectors = 0; 
+int32_t RootDirSectors = 0;
 int32_t FirstDataSector = 0;
 int32_t FirstSectorofCluster = 0;
 int32_t root_address;
-char file_open[MAX_IMAGE_FILE_NAME][MAX_COMMAND_SIZE];
+char file_open[MAX_SIZE_FILE][MAX_COMMAND_SIZE];
 int file_counter = 0;
 int open_file = 0;
 
 int LBAToOffset(int sector)
 {
-  return ((sector -2) * BPB_BytesPerSec)+(BPB_BytesPerSec* BPB_RsvdSecCnt)+(BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
+  return ((sector - 2) * BPB_BytesPerSec) + (BPB_BytesPerSec * BPB_RsvdSecCnt) + (BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
 }
 
-int compare(char IMG_Name[MAX_IMAGE_FILE_NAME],char input[MAX_IMAGE_FILE_NAME] )
+int compare(char *IMG_Name, char *input)
 {
   char expanded_name[12];
-  memset( expanded_name, ' ', 12 );
+  memset(expanded_name, ' ', 12);
 
-  char *token = strtok( input, "." );
+  char *token = strtok(input, ".");
 
-  strncpy( expanded_name, token, strlen( token ) );
+  strncpy(expanded_name, token, strlen(token));
 
-  token = strtok( NULL, "." );
+  token = strtok(NULL, ".");
 
-  if( token )
+  if (token)
   {
-    strncpy( (char*)(expanded_name+8), token, strlen(token ) );
+    strncpy((char *)(expanded_name + 8), token, strlen(token));
   }
 
   expanded_name[11] = '\0';
 
   int i;
-  for( i = 0; i < 11; i++ )
+  for (i = 0; i < 11; i++)
   {
-    expanded_name[i] = toupper( expanded_name[i] );
+    expanded_name[i] = toupper(expanded_name[i]);
   }
 
-  if( strncmp( expanded_name, IMG_Name, 11 ) == 0 )
+  if (strncmp(expanded_name, IMG_Name, 11) == 0)
   {
-    printf("They matched\n");
+    return 1;
   }
-
-  return 0;
+  else
+  {
+    return 0;
+  }
 }
 
 void decToHex(int decimal, char *temp)
 {
   char hexadecimal[150];
-  int i ,j = 0;
+  int i, j = 0;
   int remainder = 0;
-  printf("%s : ",temp);
-  while(decimal!=0)
+  printf("%s : ", temp);
+  while (decimal != 0)
   {
-    remainder = decimal%16;
-    if(remainder<10)
+    remainder = decimal % 16;
+    if (remainder < 10)
     {
       hexadecimal[i] = 48 + remainder;
       i++;
     }
     else
     {
-      hexadecimal[i]= 55 + remainder;
+      hexadecimal[i] = 55 + remainder;
       i++;
     }
-    decimal = decimal/16;
+    decimal = decimal / 16;
   }
-  for(j=i-1; j>= 0; j--)
+  for (j = i - 1; j >= 0; j--)
   {
-    printf("%c",hexadecimal[j]);
+    printf("%c", hexadecimal[j]);
   }
   printf("\n");
 }
@@ -122,30 +124,31 @@ int main()
 {
   struct DirectoryEntry dir[16];
 
-  char * cmd_str = (char*) malloc( MAX_COMMAND_SIZE );
+  char *cmd_str = (char *)malloc(MAX_COMMAND_SIZE);
 
-  while( 1 )
+  while (1)
   {
     // Print out the mfs prompt
-    printf ("mfs> ");
+    printf("mfs> ");
 
     // Read the command from the commandline.  The
     // maximum command that will be read is MAX_COMMAND_SIZE
     // This while command will wait here until the user
     // inputs something since fgets returns NULL when there
     // is no input
-    while( !fgets (cmd_str, MAX_COMMAND_SIZE, stdin) );
+    while (!fgets(cmd_str, MAX_COMMAND_SIZE, stdin))
+      ;
 
     /* Parse input */
     char *token[MAX_NUM_ARGUMENTS];
 
-    int   token_count = 0;                                 
-                                                           
+    int token_count = 0;
+
     // Pointer to point to the token
     // parsed by strsep
-    char *arg_ptr;                                         
-                                                           
-    char *working_str  = strdup( cmd_str );                
+    char *arg_ptr;
+
+    char *working_str = strdup(cmd_str);
 
     // we are going to move the working_str pointer so
     // keep track of its original value so we can deallocate
@@ -153,47 +156,47 @@ int main()
     char *working_root = working_str;
 
     // Tokenize the input stringswith whitespace used as the delimiter
-    while ( ( (arg_ptr = strsep(&working_str, WHITESPACE ) ) != NULL) && 
-              (token_count<MAX_NUM_ARGUMENTS))
+    while (((arg_ptr = strsep(&working_str, WHITESPACE)) != NULL) &&
+           (token_count < MAX_NUM_ARGUMENTS))
     {
-      token[token_count] = strndup( arg_ptr, MAX_COMMAND_SIZE );
-      if( strlen( token[token_count] ) == 0 )
+      token[token_count] = strndup(arg_ptr, MAX_COMMAND_SIZE);
+      if (strlen(token[token_count]) == 0)
       {
         token[token_count] = NULL;
       }
-        token_count++;
+      token_count++;
     }
 
     // Now print the tokenized input as a debug check
     // \TODO Remove this code and replace with your shell functionality
 
     // int token_index  = 0;
-    // for( token_index = 0; token_index < token_count; token_index ++ ) 
+    // for( token_index = 0; token_index < token_count; token_index ++ )
     // {
-    //   printf("token[%d] = %s\n", token_index, token[token_index] );  
+    //   printf("token[%d] = %s\n", token_index, token[token_index] );
     // }
-    
-    if(token[0] == NULL)
+
+    if (token[0] == NULL)
     {
       continue;
     }
 
-    else if(strcmp("open",token[0])== 0)
+    else if (strcmp("open", token[0]) == 0)
     {
-      if(token[1]==NULL)
+      if (token[1] == NULL)
       {
         printf("Error: Please enter the name of file to open.\n");
         continue;
       }
       else
       {
-        ptr_file = fopen(token[1],"r");
-        if(ptr_file == NULL)
+        ptr_file = fopen(token[1], "r");
+        if (ptr_file == NULL)
         {
           printf("Error: No file system with the given name is found.\n");
           continue;
         }
-        else if(open_file == 1)
+        else if (open_file == 1)
         {
           printf("Error: File system with the given names is already open.\n");
           continue;
@@ -201,14 +204,14 @@ int main()
         else
         {
           file_counter++;
-          fseek(ptr_file,11,SEEK_SET);
+          fseek(ptr_file, 11, SEEK_SET);
           fread(&BPB_BytesPerSec, 2, 1, ptr_file);
 
           fseek(ptr_file, 13, SEEK_SET);
           fread(&BPB_SecPerClus, 1, 1, ptr_file);
 
           fseek(ptr_file, 14, SEEK_SET);
-          fread(&BPB_RsvdSecCnt, 1, 2, ptr_file); 
+          fread(&BPB_RsvdSecCnt, 1, 2, ptr_file);
 
           fseek(ptr_file, 16, SEEK_SET);
           fread(&BPB_NumFATs, 1, 1, ptr_file);
@@ -216,75 +219,135 @@ int main()
           fseek(ptr_file, 36, SEEK_SET);
           fread(&BPB_FATSz32, 1, 4, ptr_file);
 
-          root_address = (BPB_BytesPerSec* BPB_RsvdSecCnt)+(BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
+          root_address = (BPB_BytesPerSec * BPB_RsvdSecCnt) + (BPB_NumFATs * BPB_FATSz32 * BPB_BytesPerSec);
 
-          fseek(ptr_file,root_address,SEEK_SET);
+          fseek(ptr_file, root_address, SEEK_SET);
           int i = 0;
-          for(i = 0; i < 16; i++)
+          for (i = 0; i < 16; i++)
           {
-            fread(&dir[i],sizeof(dir[i]),1,ptr_file);  
+            fread(&dir[i], sizeof(dir[i]), 1, ptr_file);
           }
 
           //printf("%d\n",file_open);
           printf("File successfully opened!!\n");
-          for(i= 0; i < file_counter; i++)
+          for (i = 0; i < file_counter; i++)
           {
-            strcpy(file_open[i],token[1]);
-          }  
+            strcpy(file_open[i], token[1]);
+          }
           open_file = 1;
-          //printf("%d\n",file_open);  
+          //printf("%d\n",file_open);
         }
       }
     }
 
-    else if(strcmp("close",token[0])== 0)
+    else if (strcmp("close", token[0]) == 0)
     {
-      int i = 0;
-      for(i = 0; i < file_counter; i++)
+      int close = 0;
+      if (token[1] == NULL)
       {
-        if(token[1] == NULL || strcmp(file_open[i],token[1]) !=0)
+        printf("Error: No file system with the given name is open.\n");
+      }
+      else
+      {
+        int i = 0;
+        for (i = 0; i < file_counter; i++)
         {
-          printf("Error: No file system with the given name is open.\n");
-          continue;
+          if (strcmp(file_open[i], token[1]) == 0)
+          {
+            close = 1;
+            break;
+          }
+        }
+        if (close == 1 && i < file_counter)
+        {
+          file_counter--;
+          int j = 0;
+          for (j = i; j < file_counter; j++)
+          {
+            file_open[j][MAX_COMMAND_SIZE] = file_open[j + 1][MAX_COMMAND_SIZE];
+          }
+          fclose(ptr_file);
+          open_file = 0;
+          printf("File successfully closed!!\n");
         }
         else
         {
-          fclose(ptr_file);
-          printf("File successfully closed!\n");
+          printf("Error: No file system with the given name is open.\n");
         }
       }
+      continue;
     }
 
-    else if (strcmp("info",token[0])==0)
+    else if (strcmp("info", token[0]) == 0)
     {
       printf("BPB_BytesPerSec : %d\n", BPB_BytesPerSec);
-      decToHex(BPB_BytesPerSec,"BPB_BytesPersec");
+      decToHex(BPB_BytesPerSec, "BPB_BytesPersec");
       printf("\n");
 
       printf("BPB_SecPerClus : %d\n", BPB_SecPerClus);
-      decToHex(BPB_SecPerClus,"BPB_SecPerClus");
+      decToHex(BPB_SecPerClus, "BPB_SecPerClus");
       printf("\n");
 
       printf("BPB_RsvdSecCnt : %d\n", BPB_RsvdSecCnt);
-      decToHex(BPB_RsvdSecCnt,"BPB_RsvdSecCnt");
+      decToHex(BPB_RsvdSecCnt, "BPB_RsvdSecCnt");
       printf("\n");
 
       printf("BPB_NumFATs : %d\n", BPB_NumFATs);
-      decToHex(BPB_NumFATs,"BPB_NumFATs");
+      decToHex(BPB_NumFATs, "BPB_NumFATs");
       printf("\n");
 
       printf("BPB_FATSz32 : %d\n", BPB_FATSz32);
-      decToHex(BPB_FATSz32,"BPB_FATSz32");
+      decToHex(BPB_FATSz32, "BPB_FATSz32");
       printf("\n");
-      
+
       continue;
     }
-    else if(strcmp("ls",token[0])==0)
+    else if (strcmp("stat", token[0]) == 0)
     {
+      // int i;
+      // int found = -1;
+      // for (i = 0; i < 16; i++)
+      // {
+      //   if (dir[i].DIR_Attr == 0x01 || dir[i].DIR_Attr == 0x10 || dir[i].DIR_Attr == 0x20)
+      //   {
+      //     char temp[100];
+      //     strcpy(temp, token[1]);
 
+      //     char temp2[12];
+      //     memset(&temp2, 0, 12);
+      //     strncpy(temp2, dir[i].DIR_Name, 11);
+
+      //     if (!strcmp(token[1], ".") || !strcmp(token[1], ".."))
+      //     {
+      //       if (strstr(dir[i].DIR_Name, token[1]) != NULL)
+      //       {
+      //         // If the parent directory is the root directory, set the low cluster to 2.
+      //         if (dir[i].DIR_FirstClusterLow == 0)
+      //         {
+      //           dir[i].DIR_FirstClusterLow = 2;
+      //         }
+
+      //         printf("DIR_Name: %22s\nDIR_Attr: %13d\nDIR_FirstClusterLow: %d\nDIR_FileSize: %11d\n", temp2, dir[i].DIR_Attr, dir[i].DIR_FirstClusterLow, dir[i].DIR_FileSize);
+      //         found = 1;
+      //         break;
+      //       }
+      //     }
+
+      //     else if (compare(dir[i].DIR_Name, temp))
+      //     {
+      //       printf("DIR_Name: %22s\nDIR_Attr: %13d\nDIR_FirstClusterLow: %d\nDIR_FileSize: %11d\n", temp2, dir[i].DIR_Attr, dir[i].DIR_FirstClusterLow, dir[i].DIR_FileSize);
+      //       found = 1;
+      //     }
+      //   }
+      // }
+
+      // if (found == -1)
+      // {
+      //   printf("Error: File not found.\n");
+      // }
     }
 
-    else if(strcmp("exit",token[0])== 0)
+    else if (strcmp("exit", token[0]) == 0)
     {
       printf("Bye! Exiting....\n");
       return 0;
@@ -295,10 +358,7 @@ int main()
       printf("Invalid Command.\n");
       continue;
     }
-    
-
     free(working_root);
   }
   return 0;
 }
-
